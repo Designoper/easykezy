33e6fedc3800b2bc3fa8cd2b0b9be7490ff3c2cd
vilamujordi <vilamujordijr@gmail.com>
2024-06-05 14:43:05 +0200

    Functional cart :D

---
+++ assets/js/cart.mjs
@@ -0,0 +1,102 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBrLIXRuHjdUNj6Etk61qNi_L-KhzOBegk",
+
+    authDomain: "easykezy.firebaseapp.com",
+
+    databaseURL: "https://easykezy-default-rtdb.firebaseio.com",
+
+    projectId: "easykezy",
+
+    storageBucket: "easykezy.appspot.com",
+
+    messagingSenderId: "1015448108936",
+
+    appId: "1:1015448108936:web:8b9d9c3eaa265a0826843d",
+
+    measurementId: "G-T23MDXNZ2S"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const dbRef = ref(getDatabase(app));
+
+// If cart has something
+if (sessionStorage.getItem("cartList") != null){
+    var cartList = sessionStorage.getItem("cartList");
+
+    // Get games collection
+    get(child(dbRef, "games")).then((snapshot) => {
+        if (snapshot.exists()) {
+            // Convert data to object
+            var gameList = snapshot.val();
+
+            // Convert cart data (string) to array
+            var cartGames = cartList.split(",");
+
+            console.log(cartGames);
+
+            // For total price
+            var totalPrice = 0;
+
+            // Clear default cart
+            document.getElementById("product-list").innerHTML = "";
+
+            for (const element in gameList) {
+                cartGames.forEach(cartGame => {
+                    if (cartGame == element) {
+                        document.getElementById("product-list").innerHTML += `
+                        <article class="product-item">
+                            <figure class="cart-photo" style="background-image: url('./assets/img/videojuegos/${gameList[element].image}');"></figure>
+                            <div class="cart-info">
+                                <h4>${gameList[element].title}</h4>
+                                <p>${gameList[element].price}€</p>
+                            </div>
+                        </article>
+                    `
+
+                        totalPrice += Number(gameList[element].price);
+
+                        console.log("cartGame = " + cartGame + " x element = " + element);
+                    }
+                });
+            }
+
+            document.getElementById("totalPrice").innerHTML = totalPrice + "€";
+
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+else {
+    document.getElementById("product-list").innerHTML = "No hay artículos en la cesta"
+    document.getElementById("totalPrice").innerHTML = ""
+}
+
+
+window.clearCartList = function(){
+    sessionStorage.removeItem("cartList");
+
+    window.location.href = "cart.html";
+}
