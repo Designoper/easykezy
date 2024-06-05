b202b30b3b82ef4b9fe68a9aeb4a6fa8d38fefd1
a <a>
2024-06-05 12:48:53 +0200

    Fixed typo

--- assets/js/login.mjs
+++ assets/js/login.mjs
@@ -61,16 +61,16 @@ function signIn() {
     // Existing and future Auth states are now persisted in the current
     // session only. Closing the window would clear any existing state even
     // if a user forgets to sign out.
-    
+
     return signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in 
+      // Signed in
       window.location.href="index.html";
     })
     .catch((error) => {
       const errorCode = error.code;
       if(errorCode == "auth/invalid-email"){
-        emailerror.innerHTML = "El mail introducido no esta regristado";
+        emailerror.innerHTML = "El mail introducido no esta registrado";
         const errorMessage = error.message;
         console.log(errorMessage + " Error Code: " + errorCode);
       }
@@ -88,7 +88,7 @@ function signIn() {
         const errorMessage = error.message;
         console.log(errorMessage + " Error Code: " + errorCode);
       }
-      
+
     });
   })
   .catch((error) => {
@@ -97,7 +97,7 @@ function signIn() {
     console.log(errorMessage + " Error Code: " + errorCode);
   });
 
-  
+
 }
 
 button.onclick = () => signIn();
